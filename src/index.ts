882843716e86551f52adc6a274655c01280fe663
Łukasz Furlepa <lucas.furlepa@gmail.com>
2019-05-18 22:34:30 +0200

    added missing sqmatrix export

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export * from './core/primitives/vector';
 export * from './core/primitives/line';
+export * from './core/primitives/sq-matrix';
 export * from './core/nullable';
 export * from './core/const';
